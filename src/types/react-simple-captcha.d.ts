c5f05a63efb9353e713fe5d3a235614653072ab0
Muchammad Muchib Zainul Fikry <muchammadmuchib@gmail.com>
2025-12-11 08:46:24 +0800

    fix: remove deprecated react-simple-captcha and use native implementation to fix vercel build

--- src/types/react-simple-captcha.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'react-simple-captcha' {
-  export function loadCaptchaEnginge(numberOfCharacters: number): void;
-  export function LoadCanvasTemplate(props: { reloadText: string }): JSX.Element;
-  export function validateCaptcha(captchaInput: string): boolean;
-}
